4bcdc292657b19e48b2229a5655130e1ad94ef6d
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2024-09-28 16:53:22 +0300

    add run command for unix

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-const char* vertexShaderSource = R"(
+const char *vertexShaderSource = R"(
     #version 330 core
     layout (location = 0) in vec2 aPos;
     uniform float uRotation;
@@ -15,7 +15,7 @@ const char* vertexShaderSource = R"(
     }
 )";
 
-const char* fragmentShaderSource = R"(
+const char *fragmentShaderSource = R"(
     #version 330 core
     out vec4 FragColor;
     void main()
@@ -24,19 +24,18 @@ const char* fragmentShaderSource = R"(
     }
 )";
 
-
-static void errorCallback(int error, const char* description)
+static void errorCallback(int error, const char *description)
 {
     std::cerr << "GLFW Error " << error << ": " << description << std::endl;
 }
 
-static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+static void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
-GLuint createShader(GLenum type, const char* source)
+GLuint createShader(GLenum type, const char *source)
 {
     GLuint shader = glCreateShader(type);
     glShaderSource(shader, 1, &source, nullptr);
@@ -68,7 +67,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(1920, 1080, "My Title", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(720, 480, "My Title", NULL, NULL);
     if (!window)
     {
         std::cerr << "Failed to create GLFW window" << std::endl;
@@ -107,9 +106,8 @@ int main()
 
     float vertices[] = {
         -0.5f, -0.5f,
-         0.5f, -0.5f,
-         0.0f,  0.5f
-    };
+        0.5f, -0.5f,
+        0.0f, 0.5f};
 
     GLuint VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -120,7 +118,7 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
